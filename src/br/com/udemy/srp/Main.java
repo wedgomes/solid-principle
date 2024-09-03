1bd272f355b9e399395ca772e512893ee7c8270d
Wédson Gomes <40505932+wedgomes@users.noreply.github.com>
2024-09-03 12:25:05 -0300

    first commit

---
+++ src/br/com/udemy/srp/Main.java
@@ -0,0 +1,25 @@
+package br.com.udemy.srp;
+
+public class Main {
+        public static void main(String[] args) {
+            System.out.println("Hello world!");
+        }
+
+        public void listClient(){
+
+        }
+
+        public void addClient(){
+
+        }
+
+        public void updateClient(){
+
+        }
+
+        public void deleteClient(){
+
+        }
+}
+
+
---
+++ src/br/com/udemy/srp/utility/Notify.java
@@ -0,0 +1,8 @@
+package br.com.udemy.srp.utility;
+
+public class Notify {
+    
+    public void notifyClient(){
+
+    }
+}
